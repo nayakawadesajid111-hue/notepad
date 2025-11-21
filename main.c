4ddccb9c96610299e32101c3ad8f7edb315a18d4
nayakawadesajid111-hue <nayakawadesajid111@gmail.com>
2025-11-21 14:27:27 +0000

    Menu

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
-/*Project name : Note pad using c language*/
+/*Project name : Notepad using c language*/
+#include <stdio.h>
+
+int main()
+{
+    int option = 0;
+    do
+    {
+        printf("%32s","Notepad\n");
+        printf("1. New\n");
+        printf("2. Open\n");
+        printf("3. Delete\n");
+        printf("4. Exit\n");
+        printf("Enter option : ");
+        scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+            // New txt file creation
+            break;
+        case 2:
+            // opening file
+            break;
+        case 3:
+            // Delete the file
+            break;
+        case 4:
+            // Exit
+            break;
+        default:
+            printf("Invalid option\n");
+            break;
+        }
+    } while (option > 6);
+}
